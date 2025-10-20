9207f5cb47019fa4249060c65eeab2bfac94c754
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-10-19 21:34:36 -0300

    feat: add initial sign up page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+
+import HomePage from './pages/home.jsx'
+import LoginPage from './pages/login.jsx'
+import NotFoundPage from './pages/not-found.jsx'
+import SignUp from './pages/signUp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )
