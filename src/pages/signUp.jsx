7f6ef8e33b549c462a566d313b750585f9daea57
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-10-21 13:55:46 -0300

    feat: ensure password and password confirmation are the same on sign up form

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -28,32 +28,37 @@ import {
 import { Input } from '@/components/ui/input.jsx'
 import { api } from '@/lib/axios'
 
-const signupSchema = z.object({
-  firstName: z.string().trim().min(1, {
-    message: 'O nome é obrigatório.',
-  }),
-  lastName: z.string().trim().min(1, {
-    message: 'O sobrenome é obrigatório.',
-  }),
-  email: z
-    .string()
-    .email({
-      message: 'O e-mail é invalido.',
-    })
-    .trim()
-    .min(1, {
-      message: 'O e-mail é invalido.',
+const signupSchema = z
+  .object({
+    firstName: z.string().trim().min(1, {
+      message: 'O nome é obrigatório.',
     }),
-  password: z.string().trim().min(6, {
-    message: 'A senha deve ter no minimo 6 caracteres.',
-  }),
-  passwordConfirmation: z.string().trim().min(6, {
-    message: 'A confirmação de senha é obrigatória.',
-  }),
-  terms: z.boolean().refine((value) => value === true, {
-    message: 'Você precisa aceitar os termos.',
-  }),
-})
+    lastName: z.string().trim().min(1, {
+      message: 'O sobrenome é obrigatório.',
+    }),
+    email: z
+      .string()
+      .email({
+        message: 'O e-mail é invalido.',
+      })
+      .trim()
+      .min(1, {
+        message: 'O e-mail é invalido.',
+      }),
+    password: z.string().trim().min(6, {
+      message: 'A senha deve ter no minimo 6 caracteres.',
+    }),
+    passwordConfirmation: z.string().trim().min(6, {
+      message: 'A confirmação de senha é obrigatória.',
+    }),
+    terms: z.boolean().refine((value) => value === true, {
+      message: 'Você precisa aceitar os termos.',
+    }),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: 'As senhas não coincidem',
+    path: ['passwordConfirmation'],
+  })
 
 const SignUpPage = () => {
   const [user, setUser] = useState()
