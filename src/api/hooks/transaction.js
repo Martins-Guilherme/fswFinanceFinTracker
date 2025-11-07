c5fce356f205aaeed0241df6aca9559af8c3cf28
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-11-06 22:05:07 -0300

    refactor: add create transaction hook

---
+++ src/api/hooks/transaction.js
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+import { useAuthContext } from '@/contexts/auth'
+
+import { TransactionService } from '../services/transaction'
+import { getBalanceQueryKey } from './user'
+
+export const createTransactionKey = ['createTransaction']
+
+export const useCreateTransaction = () => {
+  const queryClient = useQueryClient()
+
+  const { user } = useAuthContext()
+  return useMutation({
+    mutationKey: createTransactionKey,
+    mutationFn: (input) => {
+      ;(TransactionService.create(input),
+        {
+          onSuccess: () => {
+            queryClient.invalidateQueries({
+              queryKey: getBalanceQueryKey({ userId: user.id }),
+            })
+          },
+        })
+    },
+  })
+}
