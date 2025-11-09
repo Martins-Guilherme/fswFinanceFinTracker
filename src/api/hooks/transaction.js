b22f7d99fbb7a8a5fb58a23bd16c446036edb6b0
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-11-09 12:16:16 -0300

    feat: add initial transactions table

--- src/api/hooks/transaction.js
+++ src/api/hooks/transaction.js
@@ -18,6 +18,9 @@ export const useCreateTransaction = () => {
       queryClient.invalidateQueries({
         queryKey: getBalanceQueryKey({ userId: user.id }),
       })
+      queryClient.invalidateQueries({
+        queryKey: getTransactionsQueryKey({ userId: user.id }),
+      })
     },
   })
 }
@@ -33,6 +36,7 @@ export const useGetTransactions = ({ from, to }) => {
   const { user } = useAuthContext()
   return useQuery({
     queryKey: getTransactionsQueryKey({ userId: user.id, from, to }),
-    queryFn: TransactionService.getAll({ from, to }),
+    queryFn: () => TransactionService.getAll({ from, to }),
+    enabled: Boolean(from) && Boolean(to) && Boolean(user.id),
   })
 }
---
+++ src/components/transactions-table.jsx
@@ -0,0 +1,38 @@
+import { useSearchParams } from 'react-router'
+
+import { useGetTransactions } from '@/api/hooks/transaction'
+
+import { DataTable } from './ui/data-table'
+
+const columns = [
+  {
+    accessorKey: 'name',
+    header: 'Título',
+  },
+  {
+    accessorKey: 'type',
+    header: 'Tipo',
+  },
+  {
+    accessorKey: 'date',
+    header: 'Data',
+  },
+  {
+    accessorKey: 'amount',
+    header: 'Valor',
+  },
+  {
+    accessorKey: 'actions',
+    header: 'ações',
+  },
+]
+const TransactionsTable = () => {
+  const [searchParams] = useSearchParams()
+  const from = searchParams.get('from')
+  const to = searchParams.get('to')
+  const { data: transactions } = useGetTransactions({ from, to })
+  if (!transactions) return null
+  return <DataTable columns={columns} data={transactions} />
+}
+
+export default TransactionsTable
