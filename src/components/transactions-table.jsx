4c4415855a087927c5fa733b6c013687e0f203e0
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-11-09 17:15:28 -0300

    feat: add scroll and title to transactions table

--- src/components/transactions-table.jsx
+++ src/components/transactions-table.jsx
@@ -9,6 +9,7 @@ import { formatCurrency } from '@/helpers/currency'
 import TransactioTypeBadge from './transaction-type-badge'
 import { Button } from './ui/button'
 import { DataTable } from './ui/data-table'
+import { ScrollArea } from './ui/scroll-area'
 
 const columns = [
   {
@@ -56,7 +57,14 @@ const TransactionsTable = () => {
   const to = searchParams.get('to')
   const { data: transactions } = useGetTransactions({ from, to })
   if (!transactions) return null
-  return <DataTable columns={columns} data={transactions} />
+  return (
+    <>
+      <h2 className="text-2xl font-bold">Transações</h2>
+      <ScrollArea className="h-[300px] max-h-[300px] rounded-md border">
+        <DataTable columns={columns} data={transactions} />
+      </ScrollArea>
+    </>
+  )
 }
 
 export default TransactionsTable
